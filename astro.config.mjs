6208f70e3f15d4e4194440cb8f75a7415f1d6e84
Snozu <hazieleduardo@gmail.com>
2025-05-14 09:39:00 -0600

    Add configuration proyect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact'
-
+import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [preact()],
+  integrations: [preact(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
+  }
 });
